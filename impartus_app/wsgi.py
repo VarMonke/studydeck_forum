958641194e5bd4fb114cc728a75ac4e6c6acb34f
VarMonke <var.monke@gmail.com>
2026-01-05 19:00:31 +0530

    Hosting changes

--- impartus_app/wsgi.py
+++ impartus_app/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'impartus_app.settings')
 
+path = '/home/VarMonke/impartus_app'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'impartus_app.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
